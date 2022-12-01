3ebe421fca208e7466d0f59299ee46648cd32c96
Assemchik1 <118967701+Assemchik1@users.noreply.github.com>
2022-12-01 22:29:21 +0500

    задача8

---
+++ задача8/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(N > 1)
+{
+    while(evenNumber <= N)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
