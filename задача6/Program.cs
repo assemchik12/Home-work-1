d607ff12f138d8fd43bfe9796ad428ab58894931
Assemchik1 <118967701+Assemchik1@users.noreply.github.com>
2022-12-01 22:15:59 +0500

    задача6

--- задача6/Program.cs
+++ задача6/Program.cs
@@ -1,19 +1,19 @@
 ﻿// Напишите программу, которая на вход принимает
 // число и выдает, является ли число четным (делится ли оно на
 // два без остатка).
-// 2, 3, 7 -> 7
-// 44 5 78 -> 78
-// 22 3 9 -> 22
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
 
-Console.WriteLine("Введите число N = ");
-int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
 
-
-if (N % 2==1)
+int N = number % 2;
+if (N == 0)
 {
-    Console.WriteLine("число нечетное");
+    Console.WriteLine("Число четное");
 }
 else
 {
-    Console.WriteLine("Число четное");
+    Console.WriteLine("Число нечетное");
 }
